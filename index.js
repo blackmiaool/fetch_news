5ac6bdb2beec28456e574cf8fc20761eb291a667
blackmiaool <blackmiaool@gmail.com>
2016-11-08 10:34:13 +0800

    update version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fetch News
 // @namespace    http://tampermonkey.net/
-// @version      0.17
+// @version      0.18
 // @description  try to take over the world!
 // @author       You
 // @include      http://www.ifanr.com/*
